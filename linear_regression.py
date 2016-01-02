5e4252d6447e75dbdf1167359da8ced7bed5aa2b
suwangcompling <shrekwang@utexas.edu>
2016-01-02 11:31:10 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -43,6 +43,29 @@ class Lin_Reg(object):
     def get_weights(self):
         return self.theta
 
-
+'''
+Usage Example
+File: comma separated house_price.txt  
+area  num_bedrooms  price  
+2104,3,399900  
+1600,3,329900  
+2400,3,369000  
+1416,2,232000  
+3000,4,539900  
+1985,4,299900  
+...  
+'''
+>>> lr = lin_reg.Lin_Reg('house_price.txt')  
+>>> lr.cost()
+array([ 32.07273388])
+<>>> lr.get_weights()
+array([[ 0.],
+       [ 0.]])
+>>> lr.grad_desc(0.01,1500)
+>>> lr.get_weights()
+array([[-3.63029144],
+       [ 1.16636235]]
+>>> lr.cost()
+array([ 4.48338826])
 
 
