cacb769a16b7c21ee4acc3e4499cd7d44ed85027
suwangcompling <shrekwang@utexas.edu>
2016-01-02 16:52:32 -0600

    Update log_reg.py

--- log_reg.py
+++ log_reg.py
@@ -29,9 +29,9 @@ class Log_Reg(object):
         return 1.0/(1+np.exp(-prediction))
 
     # cost function
-    def cost(self,theta):
+    def cost(self,theta,lmd): # lmd: lambda, regularization coefficient
         prediction = np.dot(self.X,theta)
-        regularization = (2.0*m) * np.dot(theta.T,theta)
+        regularization = lmd/(2.0*m) * np.dot(theta.T,theta)
         J = (1.0/self.m) * (np.dot(np.log(self.sigmoid(prediction)).T,-self.y) - \
                        np.dot(np.log(1.0-self.sigmoid(prediction)).T,(1-self.y))) + \
                        regularization
